546a9da1df75812be9d555fb54cc621a0bd05279
whiteroomlz <borodinik.s@gmail.com>
2023-03-22 10:42:18 +0300

    test change

--- tests/test_spanning_tree_clustering.py
+++ tests/test_spanning_tree_clustering.py
@@ -7,12 +7,12 @@ from mst_clustering import Pipeline
 
 
 if __name__ == "__main__":
-    multiprocessing.freeze_sgitupport()
+    multiprocessing.freeze_support()
 
     X, y = make_blobs(n_samples=1000, n_features=10, centers=7)
 
     clustering = Pipeline(clustering_models=[
-        ZahnModel(3, 1.5, math.inf, max_num_of_clusters=7, use_special_criterion=False),
+        ZahnModel(3, 1.5, math.inf, max_num_of_clusters=7),
     ])
     clustering.fit(data=X, workers_count=6)
 
